2ebeca320ee04d107e197c0e880606bfa84867a6
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-11 14:48:36 +0200

    Routes params and params.snapshot

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number, name: string };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+
+    // the route changes, but we are on the same Component, so the component is not recreated by Angular
+    // it this case we need to subscribe to this to update the values in the future if they change
+
+    this.route.params
+      .subscribe((params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      });
   }
 
 }
