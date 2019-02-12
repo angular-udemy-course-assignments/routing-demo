4efdf18b03e91dfbf131cb3d00e406df89ea05aa
nkolchakov <nikola.kolchakov@gmail.com>
2019-02-12 23:13:26 +0200

    Guard for children only w/ - canActivateChild

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {ServersComponent} from './servers/servers.component';
 import {ServerComponent} from './servers/server/server.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {AuthGuard} from "./auth-guard.service";
+import {AuthGuard} from './auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -16,8 +16,10 @@ const appRoutes: Routes = [
       {path: ':id/:name', component: UserComponent}]
   },
   {
-    path: 'servers', component: ServersComponent,
-    canActivate: [AuthGuard],
+    path: 'servers',
+    component: ServersComponent,
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
